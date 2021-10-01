09dcd33c024a326af767f1a0d32f5e3c05d7b82a
ziguanl4660 <91750418+ziguanl4660@users.noreply.github.com>
2021-10-01 12:53:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Z. Li"; // update the part BETWEEN the "quotes"
+let userName = "ziguanl4660"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
